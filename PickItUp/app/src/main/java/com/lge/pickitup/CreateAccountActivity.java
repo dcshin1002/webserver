e68035db4331c5bf3090adbf097376b2823f0ae8
likepaul.kim <likepaul.kim@lge.com>
2019-10-23 13:49:19 +0900

    create new courier item when create new account

--- PickItUp/app/src/main/java/com/lge/pickitup/CreateAccountActivity.java
+++ PickItUp/app/src/main/java/com/lge/pickitup/CreateAccountActivity.java
@@ -30,9 +30,12 @@ import com.google.firebase.auth.FirebaseAuthUserCollisionException;
 import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.Arrays;
 import java.util.HashMap;
@@ -311,6 +314,42 @@ public class CreateAccountActivity extends AppCompatActivity {
         value.put(Utils.KEY_USERTYPE, dbusertype);
         value.put(Utils.KEY_USERNAME, username);
         databaseRef.child(uid).updateChildren(value);
+        makeNewCourierItemOnDB();
+
+    }
+    private void makeNewCourierItemOnDB() {
+        final DatabaseReference databaseRef = FirebaseDatabase.getInstance().getReference()
+                                                        .child(FirebaseDatabaseConnector.COURIER_REF_NAME).child(Utils.getTodayDateStr());
+
+        Query query = databaseRef.orderByChild(TmsCourierItem.KEY_SECTOR_ID);
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.getChildrenCount() == 0) {
+                    return;
+                }
+                int newCourierId = -1;
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+                    TmsCourierItem courierItem = snapshot.getValue(TmsCourierItem.class);
+                    if (courierItem.sectorid > newCourierId) {
+                        newCourierId = courierItem.sectorid;
+                    }
+                }
+                if (newCourierId != -1) {
+                    TmsCourierItem newCourier = new TmsCourierItem(++newCourierId, mEtDisplayName.getText().toString());
+                    Map<String, Object> postValues = new HashMap<>();
+                    postValues.put("/" + newCourierId , newCourier.toMap());
+                    databaseRef.updateChildren(postValues);
+                }
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     private boolean isValidPassword(String target) {
